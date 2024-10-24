260cbcf7ca8c8663b8b1090e46fcb3b7e5025977
alecanto26 <163108755+alecanto26@users.noreply.github.com>
2024-10-24 22:15:10 +0200

    Create diadelasemana.php

---
+++ diadelasemana.php
@@ -0,0 +1,30 @@
+<?php
+function diaDeLaSemana($numero) {
+    switch($numero) {
+        case 1:
+            echo "Lunes";
+            break;
+        case 2:
+            echo "Martes";
+            break;
+        case 3:
+            echo "Miércoles";
+            break;
+        case 4:
+            echo "Jueves";
+            break;
+        case 5:
+            echo "Viernes";
+            break;
+        case 6:
+            echo "Sábado";
+            break;
+        case 7:
+            echo "Domingo";
+            break;
+        default:
+            echo "Error: El número debe estar entre 1 y 7.";
+            break;
+    }
+}
+?>
